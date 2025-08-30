4dac53e3c61bd295b76b819ff6c49669233cea96
Ray Davalos <rayandredavalostp@gmail.com>
2025-08-30 10:15:19 -0500

    Botón de Home arreglado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <section className="home">
@@ -7,7 +9,7 @@ export default function Home() {
           Bienvenido a <span className="highlight">Galerias El Congreso 122-217</span>
         </h1>
         <p>Moda a tu estilo.</p>
-        <a href="/productos" className="btn">Ver Productos</a>
+        <Link to="/productos" className="btn">Ver Productos</Link>
       </div>
     </section>
   );
